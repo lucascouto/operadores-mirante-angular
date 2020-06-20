7777dadc864e1a0340231da16533815fa40d41d2
Lucas Couto <loc.unb@gmail.com>
2020-06-20 00:35:06 -0300

    criado modulo de operador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperadorComponent } from './operador/operador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperadorComponent
   ],
   imports: [
     BrowserModule,
